3e069881e2bff6a2f845819a555eae07b5653712
luxo-ai <luxo.ai@proton.me>
2023-05-25 11:53:13 -0400

    officially remove notification

--- packages/ui/src/Components/index.tsx
+++ packages/ui/src/Components/index.tsx
@@ -9,7 +9,6 @@ export * from './Dropdown';
 export * from './Input';
 export * from './List';
 export * from './Modal';
-// export * from './Notification';
 export * from './OptionGroup';
 export * from './Popup';
 export * from './Radio';
