c0dd5fe44a20dafc0ee845ad515214f236164196
luxo-ai <luxo.ai@proton.me>
2023-05-29 18:11:48 -0400

    update copy for theming

--- demo/src/components/cards/theming/copy.ts
+++ demo/src/components/cards/theming/copy.ts
@@ -1,37 +1,54 @@
 import type { CardCopy } from '../types';
 
 const themeCopy: CardCopy = {
-  title: 'Overview',
+  title: 'Theming Guide',
   description:
-    'Welcome to Rothko UI, a cutting-edge and lightweight UI component library designed to elevate your web development projects. Rothko UI stands out from the crowd by offering a sleek and modern design aesthetic that aligns perfectly with the latest trends in UI/UX.',
+    'Customize and personalize your Rothko UI components with ease using our powerful theming capabilities. Take control of the visual style, colors, typography, and more to create a cohesive and branded experience for your applications.',
   sections: {
-    features: {
-      headerVariant: 'h3',
-      headerText: 'Features',
+    creatingTheme: {
+      headerVariant: 'h2',
+      headerText: 'Creating a Theme',
       body: [
         {
-          headerVariant: 'h6',
-          headerText: 'Modern and Minimalist Design',
-          body: "Rothko UI's default styling reflects the contemporary design landscape, featuring cleaner lines and a minimalist approach. You can create sleek and sophisticated interfaces that effortlessly align with the latest design trends. The library places a strong emphasis on component design that feels natural for mobile or tablet devices, ensuring a seamless user experience across the modern web.",
+          headerVariant: 'h3',
+          headerText: 'To create a Theme',
+          body: 'To create a custom theme, simply import the `ThemeProvider` from Rothko UI and wrap your application with it. Then, provide a theme object that defines the desired styles and configurations. You can customize various aspects, such as the primary color, font family, border radius, and more.',
         },
+      ],
+    },
+    overridingStyles: {
+      headerVariant: 'h2',
+      headerText: 'Overriding Styles',
+      body: [
         {
-          headerVariant: 'h6',
-          headerText: 'Lightweight Package',
-          body: 'Rothko UI is designed with efficiency in mind. The library offers a lightweight package, ensuring fast loading times and optimal performance for your applications. You can enjoy the benefits of a robust UI component library without sacrificing speed or resource consumption.',
+          headerVariant: 'h3',
+          headerText: 'Rothko UI follows a consistent and intuitive theming approach.',
+          body: 'You can easily override specific component styles by targeting their respective keys in the theme object. This allows you to fine-tune the appearance and behavior of individual components to align with your design vision.',
         },
+      ],
+    },
+    themingVariables: {
+      headerVariant: 'h2',
+      headerText: 'Theming Variables',
+      body: [
         {
-          headerVariant: 'h6',
-          headerText: 'Strong Customization',
-          body: "Rothko UI provides extensive customization options to tailor the components to your project's specific needs. From colors and typography to layout and interaction, you have the power to create a unique and personalized UI. The library empowers developers to shape their applications according to their vision.",
+          headerVariant: 'h3',
+          headerText: 'Our theming system leverages a comprehensive set of variables.',
+          body: "This makes it effortless to update your entire application's visual style from a single source. By modifying these variables, you can achieve a consistent look and feel throughout your UI and ensure effortless maintenance as your design requirements evolve.",
         },
+      ],
+    },
+    darkLightMode: {
+      headerVariant: 'h2',
+      headerText: 'Dark Mode Support',
+      body: [
         {
-          headerVariant: 'h6',
-          headerText: 'Opinionated Default Styling',
-          body: "While Rothko UI is strongly customizable, it offers an opinionated default styling. The library's components are meticulously designed to provide a cohesive and visually striking experience out of the box. This opinionated approach saves you time and effort by offering a well-crafted starting point while still allowing room for customization.",
+          headerVariant: 'h3',
+          headerText: 'Rothko UI provides built-in support for dark mode.',
+          body: 'This allows you to create visually striking and accessible interfaces. With a simple toggle, you can switch between light and dark themes seamlessly, enhancing the user experience and adapting to user preferences.',
         },
       ],
     },
   },
 };
-
 export default themeCopy;
