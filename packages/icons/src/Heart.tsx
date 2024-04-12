b7850c0c0338164f2f22d919b490160f69df0db2
luxo-ai <luxo.ai@proton.me>
2024-04-12 05:47:02 -0400

    add title

--- packages/icons/src/Heart.tsx
+++ packages/icons/src/Heart.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import type { SVGProps, Ref } from 'react';
-import { forwardRef } from 'react';
+import { SVGProps, Ref, forwardRef } from 'react';
 const SvgHeart = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -10,7 +9,6 @@ const SvgHeart = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
     ref={ref}
     {...props}
   >
-    <title>{'<3'}</title>
     <g data-name="Layer 2">
       <path
         d="M12 21a1 1 0 0 1-.71-.29l-7.77-7.78a5.26 5.26 0 0 1 0-7.4 5.24 5.24 0 0 1 7.4 0L12 6.61l1.08-1.08a5.24 5.24 0 0 1 7.4 0 5.26 5.26 0 0 1 0 7.4l-7.77 7.78A1 1 0 0 1 12 21z"
