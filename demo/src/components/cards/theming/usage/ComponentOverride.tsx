88df1d926ae89603ef32840a379b37ff62675b6c
luxo-ai <luxo.ai@proton.me>
2024-04-15 22:16:03 -0400

    fix example

--- demo/src/components/cards/theming/usage/ComponentOverride.tsx
+++ demo/src/components/cards/theming/usage/ComponentOverride.tsx
@@ -3,28 +3,7 @@ import React from 'react';
 import { RothkoProvider } from '@rothko-ui/ui';
 
 const App = ({ children }: { children?: React.ReactNode }) => {
-  return (
-    <RothkoProvider
-      overrides={{
-        dark: {
-          slider: {
-            handle: {
-              background: { value: '#620a75' },
-            },
-          },
-        },
-        light: {
-          slider: {
-            handle: {
-              background: { value: '#f4a460' },
-            },
-          },
-        },
-      }}
-    >
-      {children}
-    </RothkoProvider>
-  );
+  return <RothkoProvider>{children}</RothkoProvider>;
 };
 
 export default App;
